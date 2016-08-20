435ed19b1599229f4546c429450ef93ce1d37d34
Waldemar Sobiecki <waldek.sob@gmail.com>
2016-08-20 09:22:50 +0200

    Dodanie do modelu Profile signal receivera. Profile jest tworzony za kazdym razem gdy tworzony jest User.

--- Project/Profile/models.py
+++ Project/Profile/models.py
@@ -3,7 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 #my imports
+#from django.contrib.auth import User
 from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
 class Profile(models.Model):
@@ -12,3 +15,8 @@ class Profile(models.Model):
 
      def __unicode__(self):
          return "%s" % (self.przezwisko)
+
+@receiver(post_save, sender = settings.AUTH_USER_MODEL)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        profile, new = Profile.objects.get_or_create(user = instance)
