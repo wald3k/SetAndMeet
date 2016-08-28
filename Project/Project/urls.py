7a9c6f5cfba3dcd947690c2e103293672d50a05e
Waldemar Sobiecki <waldek.sob@gmail.com>
2016-08-28 18:47:32 +0200

    Po Location. Pusta baza danych.

--- Project/Project/urls.py
+++ Project/Project/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('Profile.urls'),name='test'),
+    url(r'', include('Location.urls'),name='test2'),
 ]
