e354c61b0507a40d7a04b0486a2f0fa45f82e63b
Waldemar Sobiecki <waldek.sob@gmail.com>
2016-08-30 19:49:07 +0200

    Zainstalowanie Pillow potrzebnego dla imageField. Dodanie pola imageField do profilu. Pole to sluzy do przechowywania sciezki do avatara uzytkownika.

--- Project/Profile/models.py
+++ Project/Profile/models.py
@@ -10,10 +10,15 @@ from django.dispatch import receiver
 from Event.models import Event
 # Create your models here.
 
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return 'avatars/user_{0}/{1}'.format(instance.user.id, filename)
+
 class Profile(models.Model):
      user = models.OneToOneField(settings.AUTH_USER_MODEL, primary_key = True)
      przezwisko = models.CharField(max_length = 30)
-     events = models.ManyToManyField(Event)
+     events = models.ManyToManyField(Event, null=True, blank=True)
+     avatar = models.ImageField(upload_to=user_directory_path)
 
 
      def __unicode__(self):
