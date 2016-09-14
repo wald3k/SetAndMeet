f5978b4e821dfc12a5cc67cd5afe39c6987673d5
Waldemar Sobiecki <waldek.sob@gmail.com>
2016-09-14 22:02:33 +0200

    Dodanie widoku profile_list. Testy z dzialaniem odwolan do url profile_detail. Prosty szablon w html, zeby zobaczyc jak to moze wygladac.

--- Project/Profile/views.py
+++ Project/Profile/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render, redirect
 from .models import Profile
 from django.contrib.auth.decorators import login_required
 
+from django.views.generic.list import ListView
+from django.utils import timezone
+
 from django.views.generic import TemplateView
 from django.contrib.auth import logout
 
@@ -17,12 +20,21 @@ def test(request):
     return render(request,template,context)
 
 @login_required
-def profile_detail(request,profile_id):
-    p  = Profile.objects.get(id=profile_id)
+def profile_detail(request,profile_pk):
+    p  = Profile.objects.get(pk=profile_pk)
     context = {'profile':p}
     template = 'Profile/base.html'
     return render(request,template,context)
 
+
+class ProfileListView(ListView):
+    model = Profile
+    template_name = 'profile_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProfileListView, self).get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
 
 class IndexView(TemplateView):
     template_name = 'index.html'
