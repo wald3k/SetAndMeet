2d25ff30a85bb429de0d692e43580fd4daf5514c
Waldemar Sobiecki <waldek.sob@gmail.com>
2017-01-02 18:11:43 +0100

    Dodanie obslugi join event przez JS w upcoming_event_list (zielony toast/snackbar).

--- Project/static/js/snackbar.js
+++ Project/static/js/snackbar.js
@@ -2,10 +2,8 @@
  function show_snackbar(argument) {
     // Get the snackbar DIV
     var x = document.getElementById("snackbar");
-
+    var wait_in_ms = 3000;
     // Add the "show" class to DIV
-    
-    
 	    switch(argument) {
 	    case '1':
 	        x.innerHTML = 'No more spots!';
@@ -14,11 +12,16 @@
         case '2':
         	x.innerHTML = 'Rating added. Refresh page to see new average!';
         	break;
+        case '3':
+        	x.innerHTML = 'Successfully joined an event!';
+        	x.style.background="linear-gradient(#059405, #038613 60%, #036303)";
+        	wait_in_ms = 9000;
+        	break;
 	    default:
 	        x.innerHTML = "Operation unknown!";
 	        break;
 	} 
 	x.className = "show";
-    // After 3 seconds, remove the show class from DIV
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+    // After number of mili-seconds, remove the show class from DIV
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, wait_in_ms);
 }
